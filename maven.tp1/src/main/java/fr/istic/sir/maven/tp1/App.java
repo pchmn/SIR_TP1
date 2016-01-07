65659131cb3a19c6cd6fe7a4d49bd51a7d084541
clementGoachet <clement.goachet@gmail.com>
2016-01-07 08:30:33 +0100

    Init

---
+++ maven.tp1/src/main/java/fr/istic/sir/maven/tp1/App.java
@@ -0,0 +1,15 @@
+package fr.istic.sir.maven.tp1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	int test;
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+}
